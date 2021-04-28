3869da49459a0af60bf87329c65efeff298026ef
Steve <sriddiford@bglcorp.com.au>
2021-04-28 20:34:09 +1000

    init

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ echo "Generating jar file"
 
 #Copy execute_commands_on_ec2.sh file which has commands to be executed on server... Here we are copying this file
 # every time to automate this process through 'deploy.sh' so that whenever that file changes, it's taken care of
-scp -i "microec2test.pem" execute_commands_on_ec2.sh ec2-user@ec2-13-211-168-16.ap-southeast-2.compute.amazonaws.com:/home/ec2-user
+scp -i "$HOME/.ssh/microec2test.pem" execute_commands_on_ec2.sh ec2-user@ec2-13-211-168-16.ap-southeast-2.compute.amazonaws.com:/home/ec2-user
 echo "Copied latest 'execute_commands_on_ec2.sh' file from local machine to ec2 instance"
 
-scp -i "microec2test.pem" target/websockettrial-0.0.1-SNAPSHOT.jar ec2-user@ec2-13-211-168-16.ap-southeast-2.compute.amazonaws.com:/home/ec2-user
+scp -i "$HOME/.ssh/microec2test.pem" target/websockettrial-0.0.1-SNAPSHOT.jar ec2-user@ec2-13-211-168-16.ap-southeast-2.compute.amazonaws.com:/home/ec2-user
 echo "Copied jar file from local machine to ec2 instance"
 
 echo "Connecting to ec2 instance and starting server using java -jar command"
-ssh -i "microec2test.pem" ec2-user@ec2-13-211-168-16.ap-southeast-2.compute.amazonaws.com ./execute_commands_on_ec2.sh
+ssh -i "$HOME/.ssh/microec2test.pem" ec2-user@ec2-13-211-168-16.ap-southeast-2.compute.amazonaws.com ./execute_commands_on_ec2.sh
